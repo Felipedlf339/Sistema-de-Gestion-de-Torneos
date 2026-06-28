9ad84b5cbe0917b618f8064c163fac6ca77f8829
felipe <felipazo337@gmail.com>
2026-06-28 15:16:10 -0400

    feat: agregar Jugador.java, implementación de interfaz Participante

---
+++ src/org/example/modelo/Jugador.java
@@ -0,0 +1,36 @@
+package org.example.modelo;
+
+
+public class Jugador implements Participante{
+    //Datos identificadores del jugador
+    private String id;
+    private String nombre;
+    private String contacto;
+
+    /**
+     * Constructor que crea un jugador con sus datos básicos.
+     * @param id    identificador único.
+     * @param nombre nombre completo.
+     * @param contacto dato de contacto.
+     */
+    public Jugador(String id, String nombre, String contacto) {
+        this.id = id;
+        this.nombre = nombre;
+        this.contacto = contacto;
+    }
+    @Override
+    public String getId() {return id;}
+    @Override
+    public String getNombre() {return nombre;}
+    @Override
+    public int cantidadDeMiembros() {return 1;}
+    @Override
+    public String toString(){ return nombre;}
+
+
+
+
+
+
+
+}
