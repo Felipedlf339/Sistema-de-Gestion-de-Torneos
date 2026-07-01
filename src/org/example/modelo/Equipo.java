05ccf5ba6f1a6543241d826e1cc9276054ea23cf
felipe <felipazo337@gmail.com>
2026-06-30 23:30:12 -0400

    feat: agregar clase Equipo implementando interfaz Participante

--- src/org/example/modelo/Equipo.java
+++ src/org/example/modelo/Equipo.java
@@ -1,4 +1,26 @@
 package org.example.modelo;
+import java.util.List;
 
-public class Equipo {
+public class Equipo implements Participante {
+    private String id;
+    private String nombre;
+    private List<Jugador> integrantes;
+
+    public Equipo(String id, String nombreEquipo, List<Jugador> integrantes) {
+        this.id = id;
+        this.nombre = nombre;
+        this.integrantes = integrantes;
+    }
+    @Override
+    public String getId() {return id;}
+    @Override
+    public String getNombre() {return nombre;}
+    @Override
+    public int cantidadDeMiembros() {return integrantes.size();}
+
+    public List<Jugador> getIntegrantes() {return integrantes;}
+
+    @Override
+    public String toString() { return nombre;}
+
 }
